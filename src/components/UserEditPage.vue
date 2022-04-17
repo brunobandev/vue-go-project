33acf077313c5e6f315fe9e75c8d055ca4f4b8e5
Bruno Bandeira <brunoban.dev@gmail.com>
2022-04-17 14:53:59 +0100

    updating an user

--- src/components/UserEditPage.vue
+++ src/components/UserEditPage.vue
@@ -4,16 +4,132 @@
             <div class="col">
                 <h1 class="mt-3">User</h1>
             </div>
+            <hr>
+            <form-tag @userEditEvent="submitHandler" name="userForm" event="userEditEvent">
+                <text-input
+                    v-model="user.first_name"
+                    type="text"
+                    required="true"
+                    label="First Name"
+                    :value="user.first_name"
+                    name="first-name"></text-input>
+
+                <text-input
+                    v-model="user.last_name"
+                    type="text"
+                    required="true"
+                    label="Last Name"
+                    :value="user.last_name"
+                    name="last-name"></text-input>
+
+                <text-input
+                    v-model="user.email"
+                    type="email"
+                    required="true"
+                    label="Email"
+                    :value="user.email"
+                    name="email"></text-input>
+
+                <text-input 
+                    v-if="this.user.id === 0"
+                    v-model="user.password"
+                    type="password"
+                    required="true"
+                    label="Password"
+                    :value="user.password"
+                    name="password"></text-input>
+
+                <text-input 
+                    v-else
+                    v-model="user.password"
+                    type="password"
+                    label="Password"
+                    :value="user.password"
+                    name="password"></text-input>
+                
+                <hr>
+                <div class="float-start">
+                    <input type="submit" value="Save" class="btn btn-primary me-2">
+                    <router-link to="/admin/users" class="btn btn-secondary">Cancel</router-link>
+                </div>
+                <div class="float-end">
+                    <a v-if="(this.$route.params.userId > 0 ) && (parseInt(String(this.$route.params.userId), 10) !== store.user.id)" 
+                        class="btn btn-danger" href="javascript:void(0);" @click="confirmDelete(this.user.id)">Delete</a>
+                </div>
+                <div class="clearfix"></div>
+
+            </form-tag>
         </div>
     </div>
 </template>
 
 <script>
 import Security from './security.js';
+import FormTag from './forms/FormTag.vue';
+import TextInput from './forms/TextInput.vue';
+import notie from 'notie';
+import {store} from '../components/store.js';
 
 export default {
     beforeMount() {
         Security.requireToken();
+
+        if (parseInt(String(this.$route.params.userId), 10) > 0) {
+            // editing an existing user
+            // TODO - get user from database
+        }
+    },
+    data() {
+        return {
+            user: {
+                id: 0,
+                first_name: "",
+                last_name: "",
+                email: "",
+                password: "",
+            },
+            store
+        }
+    },
+    components: {
+        'form-tag': FormTag,
+        'text-input': TextInput,
+    },
+    methods: {
+        submitHandler() {
+            const payload = {
+                id: parseInt(String(this.$route.params.userId), 10),
+                first_name: this.user.first_name,
+                last_name: this.user.last_name,
+                email: this.user.email,
+                password: this.user.password,
+            }
+
+            fetch(`${process.env.VUE_APP_API_URL}/admin/users/save`, Security.requestOptions(payload))
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    notie.alert({
+                        type: "error",
+                        text: data.message
+                    })
+                } else {
+                    notie.alert({
+                        type: "success",
+                        text: "Changes saved!"
+                    })
+                }
+            })
+            .catch(error => {
+                notie.alert({
+                    type: "error",
+                    text: error
+                })
+            })
+        },
+        confirmDelete() {
+
+        }
     }
 }
 </script>
--- src/components/UsersPage.vue
+++ src/components/UsersPage.vue
@@ -4,16 +4,60 @@
             <div class="col">
                 <h1 class="mt-3">All Users</h1>
             </div>
+            <hr>
+            <table class="table table-compact table-striped">
+                <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="u in this.users" v-bind:key="u.id">
+                        <td>
+                            <router-link :to="`/admin/users/${u.id}`">{{u.last_name}}, {{u.first_name}}</router-link>
+                        </td>
+                        <td>
+                            {{u.email}}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
 
 <script>
 import Security from './security.js';
+import notie from 'notie';
 
 export default {
+    data() {
+        return {
+            users: []
+        }
+    },
     beforeMount() {
         Security.requireToken();
-    }
+
+        fetch(process.env.VUE_APP_API_URL + '/admin/users', Security.requestOptions(""))
+        .then(response => response.json())
+        .then(response => {
+            if (response.error) {
+                notie.alert({
+                    type: "error",
+                    text: response.message,
+                });
+            } else {
+                this.users = response.data.users;
+            }
+        })
+        .catch(error => {
+            notie.alert({
+                type: "error",
+                text: error,
+            });
+        })
+    },
 }
 </script>
